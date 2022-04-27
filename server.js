6784c116cc509b3d807f862ad055e428f5a1320a
Zach Davis <zacharydavis92@gmail.com>
2022-04-27 18:20:32 -0500

    'setup server'

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const db = require('./db/connection');
+const routes = require('./routes');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use('/routes', routes);
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
